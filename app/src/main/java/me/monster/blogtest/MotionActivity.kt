4763d90fc34fabb6b19856048901dbd3b2e2f2ba
jiangjiwei <jiangjiwei0322@outlook.com>
2019-07-11 11:47:07 +0800

    搭建 motionLayout 演示框架

---
+++ app/src/main/java/me/monster/blogtest/MotionActivity.kt
@@ -0,0 +1,15 @@
+package me.monster.blogtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @description
+ * @author: Created jiangjiwei in 2019-07-11 11:34
+ */
+class MotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion)
+    }
+}
---
+++ app/src/main/java/me/monster/blogtest/motion/MotionRootFragment.kt
@@ -0,0 +1,36 @@
+package me.monster.blogtest.motion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import me.monster.blogtest.R
+
+/**
+ * @description
+ * @author: Created jiangjiwei in 2019-07-11 11:31
+ */
+class MotionRootFragment : Fragment(), View.OnClickListener {
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    override fun onClick(v: View?) {
+        val id = v?.id
+        if (id == R.id.btn_motion_login) {
+            Navigation.findNavController(v)
+                    .navigate(R.id.action_motionRootFragment_to_loginFragment)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_motion_root, container, false)
+        rootView.findViewById<Button>(R.id.btn_motion_login)
+                .setOnClickListener(this)
+        return rootView
+    }
+}
