ce12133c5dc299b3e32b617b9f023dc6de6e181e
jiangjiwei <jiangjiwei0322@outlook.com>
2019-07-17 22:21:53 +0800

    内容详情页面与基础页面跳转联动

---
+++ app/src/main/java/me/monster/blogtest/fragment/DetailFragment.java
@@ -0,0 +1,22 @@
+package me.monster.blogtest.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import me.monster.blogtest.R;
+
+/**
+ * @description
+ * @author: Created jiangjiwei in 2019-07-16 23:03
+ */
+public class DetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
