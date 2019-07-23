c09be52b957d14036b02a820647627274d82c279
jiangjiwei <jiangjiwei0322@outlook.com>
2019-07-23 17:46:48 +0800

    navigation 测试状态保存状况

--- app/src/main/java/me/monster/blogtest/tab/TimerFragment.java
+++ app/src/main/java/me/monster/blogtest/tab/TimerFragment.java
@@ -1,9 +1,13 @@
 package me.monster.blogtest.tab;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,12 +20,59 @@ import me.monster.blogtest.R;
  * @author: Created jiangjiwei in 2019-06-28 23:42
  */
 public class TimerFragment extends Fragment {
+    private static final String TAG = "TimerFragment";
+    private TextView tvCounter;
+    private CountDownTimer mDownTimer = new CountDownTimer(10 * 1000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            String text = String.valueOf((millisUntilFinished / 1000) + 1);
+            tvCounter.setText(text);
+        }
+
+        @Override
+        public void onFinish() {
+            tvCounter.setText("finished");
+        }
+    };
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        Log.e(TAG, "onHiddenChanged: " + hidden);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.e(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate: ");
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Log.e(TAG, "onCreateView: ");
         View rootView = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        tvCounter = rootView.findViewById(R.id.tv_timer_down);
+        tvCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDownTimer.start();
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: ");
     }
 }
