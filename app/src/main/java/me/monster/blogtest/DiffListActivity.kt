d2d01149d5a2f4b655ae61cb636df66d1cd4582f
jiangjiwei <jiangjiwei0322@outlook.com>
2020-03-30 12:43:16 +0800

    feat: 新增多类型元素的示例

--- app/src/main/java/me/monster/blogtest/DiffListActivity.kt
+++ app/src/main/java/me/monster/blogtest/DiffListActivity.kt
@@ -1,15 +1,43 @@
 package me.monster.blogtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_diff_list.*
 import me.monster.blogtest.adapter.SimpleListAdapter
+import kotlin.random.Random
+
+const val type_text = 1
+const val type_image = 2
+const val span_count = 2
+val allImage = mutableListOf(
+    "https://cdn.pixabay.com/photo/2018/01/05/16/24/rose-3063284_150.jpg",
+    "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_150.jpg",
+    "https://cdn.pixabay.com/photo/2020/03/10/15/54/dandelion-4919334_150.jpg",
+    "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflower-3292932_150.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/08/13/15/spring-bird-2295434_150.jpg",
+    "https://cdn.pixabay.com/photo/2016/07/12/18/54/lily-1512813_150.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/10/00/41/yellow-715540_150.jpg",
+    "https://cdn.pixabay.com/photo/2012/09/08/21/51/anemone-56414_150.jpg",
+    "https://cdn.pixabay.com/photo/2017/01/11/17/27/drip-1972411_150.jpg",
+    "https://cdn.pixabay.com/photo/2017/03/15/09/00/crocus-2145539_150.jpg",
+    "https://cdn.pixabay.com/photo/2018/03/10/20/26/flowers-3215188_150.jpg",
+    "https://cdn.pixabay.com/photo/2016/07/23/00/12/sun-flower-1536088_150.jpg",
+    "https://cdn.pixabay.com/photo/2018/10/03/03/42/flower-gerbel-3720383_150.jpg",
+    "https://cdn.pixabay.com/photo/2017/12/30/13/25/portrait-3050076_150.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_150.jpg",
+    "https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729515_150.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_150.jpg",
+    "https://cdn.pixabay.com/photo/2018/09/26/21/47/flowers-3705716_150.jpg",
+    "https://cdn.pixabay.com/photo/2012/07/12/14/50/flower-52358_150.jpg",
+    "https://cdn.pixabay.com/photo/2013/05/26/12/14/rose-113735_150.jpg"
+)
 
 class DiffListActivity : AppCompatActivity() {
 
-    private val numberList = mutableListOf<SimpleText>()
+    private var numberList = mutableListOf<BaseType>()
     private val simpleListAdapter = SimpleListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,40 +55,74 @@ class DiffListActivity : AppCompatActivity() {
         btnEdit.setOnClickListener {
             edit()
         }
+        btnAddImg.setOnClickListener { addImage(1) }
 
         rv_main_diff.also {
             it.adapter = simpleListAdapter
-            it.layoutManager = LinearLayoutManager(this)
+            it.layoutManager = GridLayoutManager(this, span_count).apply {
+                this.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                    override fun getSpanSize(position: Int): Int {
+                        return if (numberList[position].type == type_image) {
+                            span_count / span_count
+                        } else {
+                            span_count
+                        }
+                    }
+                }
+            }
             it.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         }
         prepareList()
         simpleListAdapter.submitList(numberList)
     }
 
+    private fun addImage(count: Int) {
+        val copyBase = copyBase()
+        if (count == 1) {
+            copyBase.add(SimpleImage(allImage[Random(System.currentTimeMillis()).nextInt(allImage.size -1)]))
+        } else {
+            for (i in 0 until count) {
+                copyBase.add(SimpleImage(allImage[Random(System.currentTimeMillis() * i).nextInt(allImage.size -1)]))
+            }
+        }
+        numberList = copyBase
+        simpleListAdapter.submitList(copyBase)
+    }
+
     /**
      * 编辑倒数第二个元素
      */
     private fun edit() {
-        val tmpList = MutableList(10) {
-            return@MutableList numberList[it]
+        val tmpList = copyBase()
+        val baseType = tmpList[tmpList.size - 2]
+        if (baseType.type == type_text) {
+            tmpList[tmpList.size - 2] = SimpleText("New Edit To")
+            numberList = tmpList
+            simpleListAdapter.submitList(tmpList)
+        } else {
+            Toast.makeText(this, "当前倒数第二个不是文本", Toast.LENGTH_SHORT).show()
         }
-        tmpList[tmpList.size -2] = SimpleText("New Edit To")
-        simpleListAdapter.submitList(tmpList)
     }
 
     /**
      * 在列表前加上 20 个元素
      */
     private fun add() {
-        val tmpList = MutableList(10) {
-            return@MutableList numberList[it]
-        }
+        val tmpList = copyBase()
         for (i in 0 until 20) {
             val element = SimpleText(("Add Item $i"))
             tmpList.add(0, element)
             numberList.add(0, element)
         }
+        numberList = tmpList
         simpleListAdapter.submitList(tmpList)
+    }
+
+    private fun copyBase(): MutableList<BaseType> {
+        val tmpList = MutableList(numberList.size) {
+            return@MutableList numberList[it]
+        }
+        return tmpList
     }
 
     /**
@@ -71,7 +133,9 @@ class DiffListActivity : AppCompatActivity() {
             val tmpList = MutableList(numberList.size) {
                 return@MutableList numberList[it]
             }
-            simpleListAdapter.submitList(tmpList.subList(0, 10))
+            val subList = tmpList.subList(0, 10)
+            numberList = subList
+            simpleListAdapter.submitList(subList)
         }
     }
 
@@ -81,6 +145,10 @@ class DiffListActivity : AppCompatActivity() {
         }
     }
 
-    data class SimpleText(var title: String)
+    data class SimpleText(var title: String) : BaseType(type_text)
+
+    data class SimpleImage(var link: String) : BaseType(type_image)
+
+    open class BaseType(val type: Int)
 
 }
--- app/src/main/java/me/monster/blogtest/adapter/SimpleListAdapter.kt
+++ app/src/main/java/me/monster/blogtest/adapter/SimpleListAdapter.kt
@@ -4,44 +4,89 @@ import android.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import me.monster.blogtest.DiffListActivity
+import me.monster.blogtest.type_image
+import me.monster.blogtest.type_text
 
 /**
  * @description
  * @author: Created jiangjiwei in 2020/3/23 23:04
  */
 
-class SimpleDiffItemCallback : DiffUtil.ItemCallback<DiffListActivity.SimpleText>() {
-    override fun areItemsTheSame(oldItem: DiffListActivity.SimpleText, newItem: DiffListActivity.SimpleText): Boolean {
+class SimpleDiffItemCallback : DiffUtil.ItemCallback<DiffListActivity.BaseType>() {
+    override fun areItemsTheSame(
+        oldItem: DiffListActivity.BaseType,
+        newItem: DiffListActivity.BaseType
+    ): Boolean {
         return oldItem == newItem
     }
 
-    override fun areContentsTheSame(oldItem: DiffListActivity.SimpleText, newItem: DiffListActivity.SimpleText): Boolean {
-        return oldItem.title.hashCode() == newItem.title.hashCode()
+    override fun areContentsTheSame(
+        oldItem: DiffListActivity.BaseType,
+        newItem: DiffListActivity.BaseType
+    ): Boolean {
+        return if (oldItem.type != oldItem.type) {
+            false
+        } else {
+            if (oldItem.type == type_text) {
+                (oldItem as DiffListActivity.SimpleText).title.hashCode() == (newItem as DiffListActivity.SimpleText).title.hashCode()
+            } else {
+                (oldItem as DiffListActivity.SimpleImage).link.hashCode() == (newItem as DiffListActivity.SimpleImage).link.hashCode()
+            }
+        }
     }
 }
 
-class SimpleListAdapter : ListAdapter<DiffListActivity.SimpleText, SimpleListViewHolder>(
+class SimpleListAdapter : ListAdapter<DiffListActivity.BaseType, RecyclerView.ViewHolder>(
     SimpleDiffItemCallback()
 ) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleListViewHolder {
-        return SimpleListViewHolder(
-            LayoutInflater.from(
-                parent.context
-            ).inflate(R.layout.simple_list_item_1, parent, false)
-        )
+    override fun getItemViewType(position: Int): Int {
+        return getItem(position).type
     }
 
-    override fun onBindViewHolder(holder: SimpleListViewHolder, position: Int) {
-        holder.tvText.text = getItem(position).title
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == type_text) {
+            SimpleListViewHolder(
+                LayoutInflater.from(
+                    parent.context
+                ).inflate(R.layout.simple_list_item_1, parent, false)
+            )
+        } else {
+            SimpleImageViewHolder(
+                LayoutInflater.from(
+                    parent.context
+                ).inflate(me.monster.blogtest.R.layout.item_img_diff, parent, false)
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item.type == type_text) {
+            (holder as SimpleListViewHolder).tvText.text =
+                (item as DiffListActivity.SimpleText).title
+        } else if (item.type == type_image) {
+            val simpleImage = item as DiffListActivity.SimpleImage
+            val ivDiff = (holder as SimpleImageViewHolder).ivDiff
+            Glide.with(ivDiff)
+                .asBitmap()
+                .load(simpleImage.link)
+                .into(ivDiff)
+        }
     }
 }
 
-class SimpleListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class SimpleListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var tvText: TextView = itemView.findViewById(android.R.id.text1)
+}
+
+class SimpleImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val ivDiff = itemView.findViewById<ImageView>(me.monster.blogtest.R.id.ivDiff)
 }
